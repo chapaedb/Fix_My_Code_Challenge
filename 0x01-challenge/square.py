9b64b0ba5c04013b171a0e14817d774b44420a82
chapaedb <chapaeresso1@gmail.com>
2024-03-17 09:13:18 -0700

    Done

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -16,3 +16,10 @@ class Square:
 
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
+
+
+if __name__ == "__main__":
+    s = Square(width=12, height=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.perimeter_of_my_square())
